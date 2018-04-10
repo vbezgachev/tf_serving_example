63a3d7413b9d5b5d9ef4480dcadfa5bf11aad947
Vitaly Bezgachv <vitaly.bezgachev@gmail.com>
2018-04-10 21:40:44 +0200

    refactor: more appropriate print output

--- svnh_semi_supervised_model_loaded_test.py
+++ svnh_semi_supervised_model_loaded_test.py
@@ -86,7 +86,7 @@ def load_and_predict_with_saved_model():
             scores = sess.run(output_tensor, {input_tensor: [image]})
 
         # print results
-        print("Image num: {}".format(3))
+        print("Image file name: {}".format(image_file_name))
         print("Scores: {}".format(scores))
 
 
